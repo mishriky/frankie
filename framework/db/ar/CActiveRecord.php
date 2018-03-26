d2b21312e501b6b6f5467750371bb6f8b5f2d486
Mina Mishriky <m.mishriky@gmail.com>
2018-03-26 07:52:42 -0700

    update

--- framework/db/ar/CActiveRecord.php
+++ framework/db/ar/CActiveRecord.php
@@ -1148,8 +1148,16 @@ abstract class CActiveRecord extends CModel
 				$this->_pk=$this->getPrimaryKey();
 
             error_log(sprintf('pk is %s', $this->_pk));
+            error_log(sprintf('pk is %s', $this->_pk[0]));
+            error_log(sprintf('pk is %s', $this->_pk[1]));
 
-            $this->updateByPk($this->getOldPrimaryKey(),$this->getAttributes($attributes));
+            $oldPk = $this->getOldPrimaryKey();
+            error_log(sprintf('old pk is %s', $oldPk[0]));
+
+            $attr = $this->getAttributes($attributes);
+            error_log(sprintf('attr is %s', $attr[0]));
+
+            $this->updateByPk($oldPk, $attr);
             error_log(sprintf('ran update'));
 			$this->_pk=$this->getPrimaryKey();
             error_log(sprintf('pk size is %d', sizeof($this->_pk)));
